6e165a484f260794c78f2e0098200c5b2e8272de
TANGEJ0411 <a123451121@gmail.com>
2023-01-08 17:28:00 +0800

    feat:Google.icon

--- src/pages/Hotel/HotelConponent/RecommendToC/RecommendToC.js
+++ src/pages/Hotel/HotelConponent/RecommendToC/RecommendToC.js
@@ -9,8 +9,10 @@ export const RecommendToC = () => {
         <h4 className="title my-heading text-center py-4">精選推薦給您</h4>
         <div className="cards d-flex justify-content-center">
           <div className="small-card mx-3">
-            <div className="">
-              <span class="material-symbols-outlined">bookmark</span>
+            <div className="position-relative">
+              <span class="material-symbols-rounded my-p position-absolute tag">
+                bookmark
+              </span>
               <img
                 src={pic3}
                 alt="pic3"
@@ -35,30 +37,87 @@ export const RecommendToC = () => {
             </div>
           </div>
           <div className="small-card mx-3">
-            <div className="">
+            <div className="position-relative">
+              <span class="material-symbols-rounded my-p position-absolute tag on-hover">
+                bookmark
+              </span>
               <img
                 src={pic3}
                 alt="pic3"
                 className="card-pic my-border-radius"
               />
+              <h6 className="nav-foot-small my-2">娜路彎銀河酒店</h6>
+              <p className="my-p text-hide">
+                娜路彎銀河酒店位於台東市，設有全年開放的室外泳池和 SPA
+                中心。此住宿附設免費私人停車位。 此住宿距離台東火車站 10
+                分鐘車程。此住宿距離鐵花村音樂聚落和台東鐵道藝術村。
+              </p>
+              <div className="d-flex justify-content-between">
+                <div className="d-flex">
+                  <div class="material-symbols-outlined my-p">location_on</div>
+                  <p className="my-p">台東市</p>
+                </div>
+                <div className="d-flex">
+                  <div class="material-symbols-outlined my-p">paid</div>
+                  <p className="my-p">1000 / 晚</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="small-card mx-3">
-            <div className="">
+            <div className="position-relative">
+              <span class="material-symbols-rounded my-p position-absolute tag">
+                bookmark
+              </span>
               <img
                 src={pic3}
                 alt="pic3"
                 className="card-pic my-border-radius"
               />
+              <h6 className="nav-foot-small my-2">娜路彎銀河酒店</h6>
+              <p className="my-p text-hide">
+                娜路彎銀河酒店位於台東市，設有全年開放的室外泳池和 SPA
+                中心。此住宿附設免費私人停車位。 此住宿距離台東火車站 10
+                分鐘車程。此住宿距離鐵花村音樂聚落和台東鐵道藝術村。
+              </p>
+              <div className="d-flex justify-content-between">
+                <div className="d-flex">
+                  <div class="material-symbols-outlined my-p">location_on</div>
+                  <p className="my-p">台東市</p>
+                </div>
+                <div className="d-flex">
+                  <div class="material-symbols-outlined my-p">paid</div>
+                  <p className="my-p">1000 / 晚</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="small-card mx-3">
-            <div className="">
+            <div className="position-relative">
+              <span class="material-symbols-rounded my-p position-absolute tag">
+                bookmark
+              </span>
               <img
                 src={pic3}
                 alt="pic3"
                 className="card-pic my-border-radius"
               />
+              <h6 className="nav-foot-small my-2">娜路彎銀河酒店</h6>
+              <p className="my-p text-hide">
+                娜路彎銀河酒店位於台東市，設有全年開放的室外泳池和 SPA
+                中心。此住宿附設免費私人停車位。 此住宿距離台東火車站 10
+                分鐘車程。此住宿距離鐵花村音樂聚落和台東鐵道藝術村。
+              </p>
+              <div className="d-flex justify-content-between">
+                <div className="d-flex">
+                  <div class="material-symbols-outlined my-p">location_on</div>
+                  <p className="my-p">台東市</p>
+                </div>
+                <div className="d-flex">
+                  <div class="material-symbols-outlined my-p">paid</div>
+                  <p className="my-p">1000 / 晚</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
