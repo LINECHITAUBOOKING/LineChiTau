de2e7189c9368295513532837ea097bcb62631a1
kevintom <117623068+kevintom1997@users.noreply.github.com>
2023-01-10 11:06:30 +0800

    [add profile-develope setting] (#1)

--- src/pages/Profile/pages/Pay/Pay.js
+++ src/pages/Profile/pages/Pay/Pay.js
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React from 'react';
+import './Pay.scss';
+import Card1 from './images/Card1.png';
+import Card2 from './images/Card2.png';
+import Card3 from './images/Card3.png';
+import Card4 from './images/Card4.png';
+import Card5 from './images/Card5.png';
+import Card6 from './images/Card6.png';
 const Pay = () => {
   return (
-    <div>Pay</div>
-  )
-}
+    <div className="container">
+      <h1 className="buytitle valign-text-middle notosans-normal-old-copper-32px">
+        管理付款資料
+      </h1>
+      <div className="manger-profile my-border-radius">
+        <div className="pay-text">以下為已綁定的卡片</div>
+        <div className="pay-card">
+          <img className="pic" src={Card1} alt="" />
+          <img className="pic" src={Card2} alt="" />
+          <img className="pic" src={Card3} alt="" />
+        </div>
+        <div className="pay-card">
+          <img className="pic" src={Card4} alt="" />
+          <img className="pic" src={Card5} alt="" />
+          <img className="pic" src={Card6} alt="" />
+        </div>
+      </div>
+      <h1 className="buytitle valign-text-middle notosans-normal-old-copper-32px">
+        新增卡片
+      </h1>
+      <div className="manger-profile my-border-radius"></div>
+    </div>
+  );
+};
 
-export default Pay
+export default Pay;
