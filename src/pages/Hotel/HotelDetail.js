93fe1b7cca5c894dd0cd5bd6c160f68aab4bd966
TANGEJ0411 <a123451121@gmail.com>
2023-01-11 20:24:39 +0800

    style:hoteldetail compelete

--- src/pages/Hotel/HotelDetail.js
+++ src/pages/Hotel/HotelDetail.js
@@ -2,6 +2,9 @@ import React from 'react';
 import HotelDetailPic from './HotelConponent/HotelDetailPic/HotelDetailPic';
 import HotelIntro from './HotelConponent/HotelIntro/HotelIntro';
 import RoomSelect from './HotelConponent/RoomSelect/RoomSelect';
+import Transpotation from './HotelConponent/Transpotation/Transpotation';
+import RoomService from './HotelConponent/RoomService/RoomService';
+import RoomRule from './HotelConponent/RoomRule/RoomRule';
 
 const HotelDetail = () => {
   return (
@@ -9,6 +12,9 @@ const HotelDetail = () => {
       <HotelDetailPic />
       <HotelIntro />
       <RoomSelect />
+      <Transpotation />
+      <RoomService />
+      <RoomRule />
     </>
   );
 };
