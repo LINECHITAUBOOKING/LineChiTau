ced50b42c2428233f54a03d1455c37059e9aa9bd
Echelon0815 <jack.w886@gmail.com>
2023-01-13 18:40:58 +0800

    feat 開始排產品詳細頁面

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './pages/layouts/Navbar';
-import Footer from './pages/layouts/Footer';
-import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
-import Test from './test';
+// import { BrowserRouter } from 'react-router-dom';
+// import Navbar from './pages/layouts/Navbar';
+// import Footer from './pages/layouts/Footer';
+// import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
+import OthersComment from './pages/TripDetails/Comment/SubComment/OthersComment';
 
 function App() {
   return (
     <>
-      <Test />
+      {/* <BrowserRouter>
+        <Navbar />
+        <ShoppingCart />
+        <Footer />
+      </BrowserRouter> */}
+      <OthersComment />
     </>
   );
 }
--- src/test.js
+++ src/test.js
@@ -1,34 +1,27 @@
-const data = [
-  {
-    id: 107001,
-    name: '張小草',
-  },
-  {
-    id: 107002,
-    name: '陳小花',
-  },
-];
+import { useState } from 'react';
 
 function Test() {
+  // [getter, setter]
+  const [total, setTotal] = useState(0);
+
   return (
     <>
-      <h1>學生資料範例(map)</h1>
-      <hr />
-      <ul>
-        {/* key值的選擇
-            1. 資料來自資料庫，優先使用資料庫中的key(主鍵/外鍵)當key值
-            2. 資料並非來自資料庫，可在資料初始化時使用uuid或nanoid等函式庫產生key值，注意*不要*寫在元件的return(即render)中
-            3. 資料為靜態資料(意指在應用程式執行中完全不會更動)時，才能使用索引值當key值
-         */}
-        {data.map((v, i) => {
-          return (
-            <li key={v.id}>
-              {v.id} / {v.name}
-            </li>
-          );
-        })}
-      </ul>
+      <h1
+        onClick={() => {
+          setTotal(total + 1);
+        }}
+      >
+        {total}
+        {<div>目前total: {total}</div>}
+      </h1>
+      {/* 用truthy/falsy判斷 */}
+      {total && <div>目前total: {total}</div>}
+      {total !== 0 && <div>目前total: {total}</div>}
+      {total > 0 && <div>目前total: {total}</div>}
+      {/* 強制轉為布林值 */}
+      {!!total && <div>目前total: {total}</div>}
     </>
   );
 }
+
 export default Test;
