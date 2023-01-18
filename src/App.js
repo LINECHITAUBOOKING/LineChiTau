320edc08b7cb409517749e65cce6880d2c37ccaf
Lu-PY <lpy102817@gmail.com>
2023-01-18 15:01:14 +0800

    refactor: 讓分支頁面能夠在reactAPP出來

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './css/global.css';
@@ -7,6 +8,7 @@ import Hotel from './pages/Hotel/Hotel';
 import Travel from './pages/Travel/Travel';
 import Ticket from './pages/Ticket/Ticket';
 import Payment from './pages/Payment/layout/Payment';
+import ShoppingCart from './pages/Payment/ShoppingCart/ShoppingCart';
 import TravelPaymentDetail from './pages/Payment/PaymentDetail/TravelPaymentDetail';
 import TravelPaymentCheckOut from './pages/Payment/PaymentCheckOut/TravelPaymentCheckOut';
 import HotelPaymentDetail from './pages/Payment/PaymentDetail/HotelPaymentDetail';
@@ -14,8 +16,10 @@ import HotelPaymentCheckOut from './pages/Payment/PaymentCheckOut/HotelPaymentCh
 import Navbar from './pages/layouts/Navbar';
 import Footer from './pages/layouts/Footer';
 
+
 import ScrollToTop from './pages/layouts/ScrollToTop';
 function App() {
+  
   return (
     <>
       <BrowserRouter>
@@ -23,12 +27,14 @@ function App() {
         <ScrollToTop>
           <Routes>
             <Route path="/" element={<Hotel />} />
+            
             <Route path="/profile" element={<Profile />} />
             <Route path="/travel" element={<Travel />} />
             <Route path="/ticket" element={<Ticket />} />
             <Route path="/payment" element={<Payment />}>
+              <Route index path="" element={<ShoppingCart />} />
+              <Route path="ShoppingCart" element={<ShoppingCart />} />
               <Route
-                index
                 path="TravelPaymentDetail"
                 element={<TravelPaymentDetail />}
               />
