1bc14434527b8b1a5940582014433e7877b77165
kevintom <kk860621kk@gmail.com>
2023-01-19 21:04:10 +0800

    [modify demo.js]

--- src/utils/Demo.js
+++ src/utils/Demo.js
@@ -27,11 +27,93 @@ function Demo() {
 
   init(axios);
 
+  const [member, setMember] = useState({
+    name: 'tom',
+    password: '123456',
+    confirmPassword: '123456',
+    email: 'test@gmail.com',
+  });
+
+  function handleChange(e) {
+    setMember({ ...member, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    console.log('handleSubmit');
+    // 關閉表單的預設行為
+    e.preventDefault();
+    // 作法1: 沒有檔案的表單
+    // ajax
+    // let response = await axios.post('http://localhost:3001/api/auth/register', member);
+    // 作法2: 有檔案的表單
+    // https://developer.mozilla.org/en-US/docs/Web/API/FormData
+    let formData = new FormData();
+    formData.append('email', member.email);
+    formData.append('name', member.name);
+    formData.append('password', member.password);
+    formData.append('confirmPassword', member.confirmPassword);
+    formData.append('photo', member.photo);
+    let response = await axios.post('http://localhost:3001/register', member);
+    console.log(response.data);
+  }
+
   return (
     <>
       <div className="demo">
-        <div className="login">
-          <button className="firstBtn">登入/註冊</button>
+        <from className="login">
+          <div className="firstBtn">註冊</div>
+          使用第三方帳戶登入/創立帳戶
+          <div className="icon">
+            <img className="pic" src={line} alt="" />
+            <img className="pic" src={twitter} alt="" />
+            <img className="pic" src={fackbook} alt="" />
+            <img className="pic" src={google} alt="" />
+          </div>
+          <div className="email">
+            電子信箱
+            <input
+              type="text"
+              placeholder="請輸入電子信箱"
+              id="email"
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="name">
+            姓名
+            <input
+              type="text"
+              placeholder="請輸入姓名"
+              id="name"
+              name="name"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="password">
+            密碼
+            <input
+              type="password"
+              placeholder="請輸入密碼"
+              id="password"
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="confirmPassword">
+            再輸入一次密碼
+            <input
+              type="password"
+              placeholder="請再次輸入密碼"
+              id="confirmPassword"
+              name="confirmPassword"
+              onChange={handleChange}
+            />
+          </div>
+          <button className="lastBtn" onClick={handleSubmit}>
+            註冊
+          </button>
+        </from>
+        {/* <button className="firstBtn">登入/註冊</button>
           使用第三方帳戶登入/創立帳戶
           <div className="icon">
             <img className="pic" src={line} alt="" />
@@ -68,8 +150,8 @@ function Demo() {
             <span class="material-symbols-outlined">done</span>
             成功！1秒後跳轉首頁
           </div>
-          <button className="lastBtn">登入/註冊</button>
-        </div>
+          <button className="lastBtn">登入/註冊</button> */}
+
         <img className="pic" src={bgimg} alt="" />
       </div>
 
