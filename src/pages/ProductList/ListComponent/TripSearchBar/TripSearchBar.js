6f55de8f8486c35b6bec4dd9afe3e5ed4253fa5f
Echelon0815 <jack.w886@gmail.com>
2023-02-06 12:21:09 +0800

    製作searchbar

--- src/pages/ProductList/ListComponent/TripSearchBar/TripSearchBar.js
+++ src/pages/ProductList/ListComponent/TripSearchBar/TripSearchBar.js
@@ -1,27 +1,37 @@
-import './TripSearchBar.scss'
-import { useState } from 'react'
+import './TripSearchBar.scss';
+import { useState } from 'react';
 
-export default function SearchBar({setNewRawKeyword}) {
-    
+export default function SearchBar({ setNewRawKeyword }) {
+  // TODO 設定State
+  //! State 用來區分 輸入內容 和 placeholder 兩種狀態
 
-    
-    // TODO 設定State
-    //! State 用來區分 輸入內容 和 placeholder 兩種狀態
-    
+  const [isClientInputed, JudgeTheInput] = useState(false);
+  const inputStatus = isClientInputed ? 'to-input' : 'still-no-input';
+  console.log(inputStatus);
 
-    const [isClientInputed,JudgeTheInput] = useState('false')
+  // TODO 不提供自動完成功能，將輸入內容用LIST的state setter傳回去
 
-    // TODO 不提供自動完成功能，將輸入內容用LIST的state setter傳回去
-
-
-
-    return (
-        <>
-            <div className='trip-search-bar-wrapper'>
-                <h1><span className='material-symbols-outlined location'>location_on</span></h1>
-                <div className={'trip-search-bar-new-keyword' +  isClientInputed ? "client-input":"place-holder"}></div>
-                <button><span className='material-symbols-outlined'>search</span></button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="trip-search-bar-wrapper d-flex align-items-center">
+        <h1 className="seach-hint my-heading d-flex justify-content-evenly align-items-center">
+          <span className="material-symbols-outlined location location-icon d-flex justify-content-center align-items-center">
+            location_on
+          </span>
+          {isClientInputed ? 'none' : '目的地'}
+        </h1>
+        <div className="trip-search-bar-keyword-container my-p">
+          <input
+            className="trip-search-bar-new-keyword"
+            type="text"
+            id="keyword"
+            placeholder="請輸入地名和關鍵字"
+          />
+        </div>
+        <button className="search-launch-btn">
+          <span className="material-symbols-outlined">search</span>
+        </button>
+      </div>
+    </>
+  );
 }
