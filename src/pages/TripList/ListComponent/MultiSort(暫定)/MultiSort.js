a995e8aad32ed1229653a7a0d415babf0811906a
Echelon0815 <jack.w886@gmail.com>
2023-02-11 14:37:28 +0800

    完成篩選功能

---
+++ src/pages/TripList/ListComponent/MultiSort(暫定)/MultiSort.js
@@ -0,0 +1,11 @@
+export default function MultiSort() {
+  return (
+    <>
+      <div className="test">
+        <label>
+          <input type="checkbox" />
+        </label>
+      </div>
+    </>
+  );
+}
--- src/pages/TripList/TripList.js
+++ src/pages/TripList/TripList.js
@@ -1,32 +1,28 @@
 import './TripList.scss';
-import testJSON from '../../test.json';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-// import PopupSort from './ListComponent/PopupSort/PopupSort';
-import NormalSort from './ListComponent/NormalSort/NormalSort';
 import ProductsCard from './ListComponent/ProductsCard/ProductsCard';
-// import Pagination from './ListComponent/Pagination/Pagination';
 import ListMap from '../layouts/ListMap/ListMap';
-import TripSearchBar from './ListComponent/TripSearchBar/TripSearchBar';
+import FilterBox from './ListComponent/FilterBox/FilterBox';
+// import Pagination from './ListComponent/Pagination/Pagination';
+// import TripSearchBar from './ListComponent/TripSearchBar/TripSearchBar';
 // import { useParams } from 'react-router-dom';
 
 export default function TestList() {
   //! state :訪問網頁時的關鍵字、篩選條件、商品收藏
   //! 第一次渲染拿資料，接下來除非更動搜尋BAR(地名、關鍵字)，不會發requset
-  //TODO 加上排序條件、更新收藏資料庫、製作分業、加上地圖
+  //TODO 加上排序條件、更新收藏資料庫、
   //TODO 將從API調出的資料傳進去產品卡片
 
-  //利用 react-router-dom 獲得一開始的關鍵字 或著其他方式 很有可能是localstorage
-  // const { URLRawkeyword } = useParams();
   const URLRawkeyword = '台北';
-
-  const [newRawKeyword, setNewRawKeyword] = useState();
+  // const [newRawKeyword, setNewRawKeyword] = useState();
 
   //將回傳資料設為本元件的state
-  const [ReturnedTripData, setReturnedTripData] = useState([]);
-
-  //打算把子元件的state當作"篩選用"的關鍵字
-  // const [SortKeywordArr, setSortKeywordArr] = useState();
+  const [returnedTripData, setReturnedTripData] = useState([]);
+  //將回傳資料加上Service屬性並儲存
+  const [serviceAddedData, setServiceAddedData] = useState([]);
+  //最終要render成卡片的資料
+  const [dataForDisplay, setDataForDisplay] = useState([]);
 
   //!處理關鍵字的函式
 
@@ -47,7 +43,6 @@ export default function TestList() {
     });
     return [name, region];
   }
-
   function isRegion(element) {
     const regionArr = [
       '基隆',
@@ -78,7 +73,7 @@ export default function TestList() {
     }
   }
 
-  // //!藉由關鍵字獲取資料的函式
+  //!藉由關鍵字獲取資料的函式
 
   async function fetchData(regionKeywordArr, nameKeywordArr) {
     const regionKeyword =
@@ -105,8 +100,158 @@ export default function TestList() {
     }
   }
 
-  // //!渲染從搜尋關鍵字獲取的資料
-  const RenderReturnedTripData = ReturnedTripData.map((e, i) => {
+  //! filter時會使用的state
+  // 行程特色
+  const [selectedServiceFilter, setSelectedServiceFilter] = useState([]);
+
+  const tripServiceList = [
+    '人文歷史',
+    '娛樂享受',
+    '登山踏青',
+    '戲水活動',
+    '雪上活動',
+    '無購物行程',
+    '供餐',
+    '自助旅行',
+    '導遊帶隊',
+  ];
+  // 價格單選
+  const [priceRange, setPriceRange] = useState('所有');
+
+  const tripPriceArr = [
+    '所有',
+    '0001~2000',
+    '2001~4000',
+    '4001~6000',
+    '6001~8000',
+    '8001以上',
+  ];
+
+  //! filter函式
+  const handlePriceRange = (serviceAddedData, priceRange) => {
+    let filteredData = [...serviceAddedData];
+
+    switch (priceRange) {
+      case '0001~2000':
+        filteredData = serviceAddedData.filter((item) => {
+          return item.price_adu >= 1 && item.price_adu <= 2000;
+        });
+        break;
+      case '2001~4000':
+        filteredData = serviceAddedData.filter((item) => {
+          return item.price_adu >= 2001 && item.price_adu <= 4000;
+        });
+        break;
+      case '4001~6000':
+        filteredData = serviceAddedData.filter((item) => {
+          return item.price_adu >= 4001 && item.price_adu <= 6000;
+        });
+        break;
+      case '6001~8000':
+        filteredData = serviceAddedData.filter((item) => {
+          return item.price_adu >= 6001 && item.price_adu <= 8000;
+        });
+        break;
+      case '8001以上':
+        filteredData = serviceAddedData.filter((item) => {
+          return (
+            item.price_adu >= 8001 && item.price_adu <= Number.MAX_SAFE_INTEGER
+          );
+        });
+        break;
+      default:
+        break;
+    }
+
+    return filteredData;
+  };
+  const handleSelectedServiceFilter = (
+    serviceAddedData,
+    selectedServiceFilter
+  ) => {
+    let filteredData = [...serviceAddedData];
+    if (selectedServiceFilter.length > 0) {
+      filteredData = [...filteredData].filter((item) => {
+        let isFound = false;
+
+        const itemService = item.service.join('');
+
+        for (let i = 0; i < selectedServiceFilter.length; i++) {
+          if (itemService.includes(selectedServiceFilter[i])) {
+            isFound = true;
+            break;
+          }
+        }
+        return isFound;
+      });
+    }
+    return filteredData;
+  };
+  //! fetch 資料庫
+  useEffect(() => {
+    const [nameKeywordArr, regionKeywordArr] = seperateRawKeywordArr(
+      makeRawKeywordsAnArr(URLRawkeyword),
+      isRegion
+    );
+    fetchData(regionKeywordArr, nameKeywordArr);
+  }, []);
+
+  //! 將fetch過的資料加上 service 屬性
+  useEffect(() => {
+    const ArrangedData = returnedTripData.map((item, i) => {
+      const {
+        culture_history,
+        amusement,
+        meal,
+        no_shopping,
+        self_trip,
+        guide_trip,
+        mountain,
+        in_water,
+        snow,
+      } = item;
+      //替每個row創立一個陣列
+      const ItemServiceList = [
+        { service: '人文歷史', value: culture_history },
+        { service: '娛樂享受', value: amusement },
+        { service: '供餐', value: meal },
+        { service: '無購物行程', value: no_shopping },
+        { service: '自助旅行', value: self_trip },
+        { service: '導遊帶隊', value: guide_trip },
+        { service: '登山踏青', value: mountain },
+        { service: '水上活動', value: in_water },
+        { service: '雪上活動', value: snow },
+      ];
+      //將這陣列篩選，並把其中的字串取出來
+      const ItemActualService = ItemServiceList.filter((v, i) => {
+        return v.value !== 0;
+      }).map((v) => {
+        return v.service;
+      });
+      //將字串加進原本的元素中
+      item.service = ItemActualService;
+      return item;
+    });
+    setServiceAddedData(ArrangedData);
+  }, [returnedTripData]);
+
+  console.log('加上sevice的Data', serviceAddedData);
+  console.log('現在的價錢篩選', priceRange);
+  console.log('現在的條件篩選', selectedServiceFilter);
+  console.log('篩選完的data', dataForDisplay);
+  //! 當資料第一次載入和當表單有更動時，開始篩選
+  useEffect(() => {
+    let filteredData = [];
+    filteredData = handlePriceRange(serviceAddedData, priceRange);
+    filteredData = handleSelectedServiceFilter(
+      filteredData,
+      selectedServiceFilter
+    );
+    setDataForDisplay(filteredData);
+  }, [priceRange, selectedServiceFilter, serviceAddedData]);
+
+  //!渲染從搜尋關鍵字獲取的資料
+  const renderReturnedTripData = returnedTripData.map((e, i) => {
     return (
       <ProductsCard
         key={i}
@@ -116,62 +261,24 @@ export default function TestList() {
       />
     );
   });
-
-  console.log(testJSON.Info);
-  const TestArr = testJSON.Info;
-
-
-  const RenderTestJSON = TestArr.map((e, i) => {
+  // 沒結果的話
+  const thereIsNoReturnedTripData = () => {
     return (
       <>
-        <p>{e.Id}</p>
+        <div className="container-fluid d-flex justify-content-center align-items-center my-topic">
+          查無結果
+        </div>
       </>
     );
-  });
-
-  // json parse 讓字串變成JSON stringift反之
-
-  //! 沒結果時 先凍結
-  // const ThereIsNoReturnedTripData = () => {
-  //   return (
-  //     <>
-  //       <div className="container-fluid d-flex justify-content-center align-items-center my-topic">
-  //         查無結果
-  //       </div>
-  //     </>
-  //   );
-  // };
-
-  //! fetch 資料庫 先凍結
-  // useEffect(() => {
-  //   const [nameKeywordArr, regionKeywordArr] = seperateRawKeywordArr(
-  //     makeRawKeywordsAnArr(URLRawkeyword),
-  //     isRegion
-  //   );
-  //   fetchData(regionKeywordArr, nameKeywordArr);
-  // }, []);
-
-  // console.log(ReturnedTripData);
-
-  //建立 篩選條件的 State
-  // const [filterForData, setFilterForData] = useState({
-  //   eventFeature: [],
-  //   MaxPrice: 0,
-  //   MinPrice: Number.MAX_SAFE_INTEGER,
-  // });
-
-  //! filter 先凍結
-  // useEffect(() => {
-  //   console.log(filterForData);
-  // }, [filterForData]);
+  };
 
   return (
     <>
-      <TripSearchBar setNewRawKeyword={setNewRawKeyword} />
+      {/* <TripSearchBar setNewRawKeyword={setNewRawKeyword} /> */}
       <div className="page-wrapper container-xl">
         <div className="result-sort d-flex justify-content-between align-items-end">
           <p className="result my-topic">
-            關鍵字：共 {ReturnedTripData ? ReturnedTripData.length : 0} 項 結果
+            關鍵字：共 {dataForDisplay ? dataForDisplay.length : 0} 項 結果
           </p>
           <ul className="top-sort-list list-unstyled d-flex my-p">
             <li className="top-sort-btn">排序：</li>
@@ -191,70 +298,19 @@ export default function TestList() {
               <div className="filter-container my-border-radius m-auto mt-3">
                 <div className="filter-box px-5 py-3">
                   <h5 className="filter-box-title nav-foot">目的地</h5>
-                  {/* <PopupSort
-                    label="北部"
-                    area="northern"
-                    setNewRawKeyword={setNewRawKeyword}
-                  />
-                  <PopupSort
-                    label="中部"
-                    area="central"
-                    setNewRawKeyword={setNewRawKeyword}
-                  />
-                  <PopupSort
-                    label="南部"
-                    area="southern"
-                    setNewRawKeyword={setNewRawKeyword}
-                  />
-                  <PopupSort
-                    label="東部"
-                    area="eastern"
-                    setNewRawKeyword={setNewRawKeyword}
-                  />
-                  <PopupSort
-                    label="離島"
-                    area="island"
-                    setNewRawKeyword={setNewRawKeyword}
-                  /> */}
-                </div>
-                <div className="filter-box px-5 py-3">
-                  <h5 className="filter-box-title nav-foot">活動特色</h5>
-                  <NormalSort label="人文歷史" id="culture-history" />
-                  <NormalSort label="娛樂享受" id="amusement" />
-                  <NormalSort label="登山踏青" id="meal" />
-                  <NormalSort label="戲水活動" id="in_water" />
-                  <NormalSort label="雪上活動" id="snow" />
                 </div>
               </div>
-              <div className="filter-container my-border-radius m-auto mt-3">
-                <div className="filter-box px-5 py-3">
-                  <h5 className="filter-box-title nav-foot">價格</h5>
-                  <NormalSort label="0 - 2000" id="overZero" />
-                  <NormalSort label="2001 - 4000" id="overTwoK" />
-                  <NormalSort label="4001 - 6000" id="overFourK" />
-                  <NormalSort label="6001 - 8000" id="overSixK" />
-                  <NormalSort label="8001 - 10000" id="overEightK" />
-                  <NormalSort label="10001 以上" id="overTenK" />
-                </div>
-              </div>
-              <div className="filter-container my-border-radius m-auto mt-3">
-                <div className="filter-box px-5 py-3">
-                  <h5 className="filter-box-title nav-foot">其他</h5>
-                  <NormalSort label="無購物行程" id="no_shopping" />
-                  <NormalSort label="供餐" id="meal" />
-                  <NormalSort label="自助旅行" id="self_trip" />
-                  <NormalSort label="導遊帶隊" id="guide_trip" />
-                </div>
-              </div>
+              <FilterBox
+                priceRange={priceRange}
+                setPriceRange={setPriceRange}
+                tripPriceArr={tripPriceArr}
+                selectedServiceFilter={selectedServiceFilter}
+                setSelectedServiceFilter={setSelectedServiceFilter}
+                tripServiceList={tripServiceList}
+              />
             </div>
           </div>
-          <div className="col-9 ">
-            {/* {ReturnedTripData
-              ? RenderReturnedTripData
-              : ThereIsNoReturnedTripData()}
-            <Pagination /> */}
-            {RenderTestJSON}
-          </div>
+          <div className="col-9 ">{/* <Pagination /> */}</div>
         </div>
       </div>
     </>
