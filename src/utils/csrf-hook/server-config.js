61fc34d8c43e606994e0246b304bfe2d0ab8e404
linjerry777 <iii20220906linjerry@gmail.com>
2023-02-13 20:23:20 +0800

    webpack

--- src/utils/csrf-hook/server-config.js
+++ src/utils/csrf-hook/server-config.js
@@ -1,6 +1,7 @@
 export const csrfTokenUrl = '/auth/csrf-token';
 export const jwtTokenUrl = '/auth/jwt-token';
 export const loginUrl = '/auth/login';
+// export const loginUrl = 'http://localhost:3001/auth/login';
 export const googleUrl = '/auth/google';
 export const registerUrl = '/auth/register';
 export const logoutUrl = '/auth/logout';
